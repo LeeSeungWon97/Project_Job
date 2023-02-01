9fae72f2461e382671c239bc04812e160fdb9553
hhhsssppp123 <hesuha98@naver.com>
2023-02-02 08:53:56 +0900

    employment 춛가
    ㄴ

--- src/main/java/com/Project_Job/controller/HomeController.java
+++ src/main/java/com/Project_Job/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.Project_Job.controller;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.Project_Job.service.EmploymentService;
 
 /**
  * Handles requests for the application home page.
@@ -14,5 +20,21 @@ public class HomeController {
 		System.out.println("메인페이지 이동 요청");
 		return "Main";
 	}
+	@Autowired
+	private EmploymentService epsvc;
+	
+	
+	
+	@RequestMapping(value = "/jobInsert2")
+	public ModelAndView jobInsert2() throws IOException {
+		System.out.println("/jobInsert2 요청");
+		int insertResult = epsvc.jobInsert2();
+		System.out.println(insertResult);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("home");
+		return mav;
+	}
+	
+	
 	
 }
